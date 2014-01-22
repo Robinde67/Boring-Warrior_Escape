d1dfde0fcb98894d7133755dfcc72406f1aa9bed
Robinde67 <a>
2014-01-22 23:06:01 +0100

    proper main

--- Escape_V2/Escape_V2/main.cpp
+++ Escape_V2/Escape_V2/main.cpp
@@ -3,80 +3,10 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-<<<<<<< HEAD
-// Here is a small helper for you ! Have a look.
 
-int main(int, char const**)
-{
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile("../Escape_V2/icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile("../Escape_V2/cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile("../Escape_V2/sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile("../Escape_V2/nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
-
-    // Start the game loop
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window : exit
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-
-            // Espace pressed : exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
-
-        // Update the window
-        window.display();
-    }
-=======
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
     std::cout << "Hello world!\n";
->>>>>>> 40e35a3e9f6277f1758ab298e9e6394d152bed3a
     
 	std::cin.get();
 	
